22db4b55ed04a01e8387ebe2b38f210bd8f8327c
TRAING Serey <t.sereypich@gmail.com>
2018-01-16 22:22:44 +0100

    add crud music

--- Spotifake/models.py
+++ Spotifake/models.py
@@ -6,5 +6,10 @@ from django.db import models
 """ Artistes """
 
 """ Musiques """
+class Music(models.Model):
+    title = models.CharField(max_length=200)
+    """album = models.ForeignKey(Album, on_delete=models.CASCADE)"""
+    def __str__(self):
+        return self.title
 
 """ Images """
